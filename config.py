75e07a86d4b862001ccc0dbf714fd44ab9a8ee49
duke96 <nguyenphankhoaduc@gmail.com>
2025-10-16 14:16:28 +0700

    Fix leak

--- config.py
+++ config.py
@@ -14,23 +14,23 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("httpcore").setLevel(logging.WARNING)
 
 # Telegram
-TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', '8265574317:AAH1xM9V9GSjesgBaSqmUvWXf96FaEDEtZc')
-INCIDENT_GROUP_ID = int(os.getenv('INCIDENT_GROUP_ID', '-1002725362613'))
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+INCIDENT_GROUP_ID = int(os.getenv('INCIDENT_GROUP_ID'))
 
 # LLM / RAG
-LLM_URL = os.getenv('LLM_URL', 'http://10.165.24.200:30424/query')
-RAG_URL = os.getenv('RAG_URL', 'http://10.159.19.9:31838/botproxy/action')
-RAG_BOT_ID = os.getenv('RAG_BOT_ID', "6df02010-a8d1-11f0-b308-c9a56fc30658")
+LLM_URL = os.getenv('LLM_URL')
+RAG_URL = os.getenv('RAG_URL')
+RAG_BOT_ID = os.getenv('RAG_BOT_ID')
 
 # Mongo
 MONGODB_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
 MONGO_DB = os.getenv('MONGO_DB', 'goconnect_bot')
 
 # MinIO
-MINIO_SERVICE_URL = os.getenv('MINIO_SERVICE_URL', 'https://voice-storage.vnpt.vn')
-MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY', 'texttospeech')
-MINIO_SECRET_KEY = os.getenv('MINIO_SECRET_KEY', 'Text2speechVnptAI@2024')
-MINIO_FOLDER_NAME = os.getenv('MINIO_FOLDER_NAME', 'emotion')  # bucket
+MINIO_SERVICE_URL = os.getenv('MINIO_SERVICE_URL')
+MINIO_ACCESS_KEY = os.getenv('MINIO_ACCESS_KEY')
+MINIO_SECRET_KEY = os.getenv('MINIO_SECRET_KEY')
+MINIO_FOLDER_NAME = os.getenv('MINIO_FOLDER_NAME')  # bucket
 MINIO_PUBLIC_HOST = os.getenv('MINIO_PUBLIC_HOST', MINIO_SERVICE_URL)
 MINIO_EXPIRE_TIME = int(os.getenv('MINIO_EXPIRE_TIME', '168')) * 3600  # hours → seconds
 
